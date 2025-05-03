046c257bae386f83af2a8b54414da3ece1dc1de7
gourav singh <gouravsinghgangola484@gmail.com>
2025-05-03 17:59:03 +0530

    frontend bug solved

--- client/EAP/src/App.jsx
+++ client/EAP/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Loginpage';
 import RegisterPage from './pages/RegisterPage';
-// import DashboardPage from './pages/DashboardPage'; // You'll create this later
+import DashboardPage from './pages/DashboardPage'; // You'll create this later
 import ProtectedRoute from './components/ProtectedRoute'; // You'll create this later
 
 export default function App() {
   return (
-    <Router>
+  
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
@@ -15,6 +15,6 @@ export default function App() {
           <Route path="/" element={<DashboardPage />} />
         </Route>
       </Routes>
-    </Router>
+    
   );
 }
